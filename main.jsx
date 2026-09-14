09018dae8187a1add03b4e6929155d555e7cbd1c
Настя <a.kotcubenko.ua@icloud.com>
2026-09-15 01:17:23 +0300

    fix: basename для роутингу

--- main.jsx
+++ main.jsx
@@ -9,7 +9,7 @@ import './app.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AuthProvider>
         <ProgressProvider>
           <App />
